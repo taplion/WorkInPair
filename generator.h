1386cbc4dcb7e594ea0c4f93d7dd3fe53ecaefdd
Tomasz.Rzepka <tomaszrzepka92@gmail.com>
2014-01-22 11:39:23 +0100

    gen modify

--- generator.h
+++ generator.h
@@ -1,8 +1,8 @@
 #include <cstdlib>
 int* generator(){
     int tab [10];
-    for(int i=0;i<9;i++){
+    for(int i=0;i<10;i++){
         tab[i]=rand()%101;
     }
-    return tab;
+    return &tab;
 }
